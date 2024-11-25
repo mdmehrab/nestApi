97ed2f2235f038929e3862d862c370c9b3066dc9
rakib <mehrabishraqhossian@gmail.com>
2024-11-25 17:39:15 +0600

    last push before adding the toking to the cokki

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserSchema } from './schema/user.schema';
-import { AuthMiddleware } from './middleware/authMiddleware'; // Import the middleware
+
 
 @Module({
   imports: [
